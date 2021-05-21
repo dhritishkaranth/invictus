9a362d34af99b90387cf1bda083c49a2e5577fcf
kumarmanjunatha <kmanjuna@uci.edu>
2021-05-20 22:07:29 -0700

    User Story 23: Search groups based on group location, illness and language

--- backend/src/main/java/uci/capstone/invictus/repository/GroupRepository.java
+++ backend/src/main/java/uci/capstone/invictus/repository/GroupRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import uci.capstone.invictus.entity.Group;
 import uci.capstone.invictus.entity.User;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,4 +17,20 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
     @Query(value = "SELECT * FROM groups WHERE ?1=ANY(languages)", nativeQuery = true)
     List<Group> findByLanguages(String language);
+
+    List<Group> findByTypeOfIllness(String illness);
+
+    List<Group> findByLocation(String location);
+
+    @Query(value = "SELECT * FROM groups WHERE typeofillness=?1 and languages && CAST(?2 AS text[]) and location=?3", nativeQuery = true)
+    List<Group> findBestMatchedGroups(String illness, String languages, String location);
+
+    List<Group> findByTypeOfIllnessAndLocation(String illness, String location);
+
+    @Query(value = "SELECT * FROM groups WHERE typeofillness=?1 AND (languages && CAST(?2 AS text[]) OR location=?3)", nativeQuery = true)
+    List<Group> findByTypeOfIllnessAndLocationOrLanguages(String illness, String languages, String location);
+
+    @Query(value = "SELECT * FROM groups WHERE typeofillness=?1 AND ?2=ANY(languages)", nativeQuery = true)
+    List<Group> findByIllnessAndLanguage(String illness, String languages);
+
 }
--- backend/src/main/java/uci/capstone/invictus/service/GroupService.java
+++ backend/src/main/java/uci/capstone/invictus/service/GroupService.java
@@ -3,33 +3,102 @@ package uci.capstone.invictus.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uci.capstone.invictus.entity.Group;
+import uci.capstone.invictus.entity.User;
 import uci.capstone.invictus.exception.GroupNotFoundException;
 import uci.capstone.invictus.exception.NoDataFoundException;
+import uci.capstone.invictus.exception.UserNotFoundException;
 import uci.capstone.invictus.repository.GroupRepository;
+import uci.capstone.invictus.repository.UserRepository;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class GroupService {
 
     @Autowired
-    private GroupRepository repository;
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public Group findByGroupName(String groupName){
-        return repository.findByGroupName(groupName).orElseThrow(()->new GroupNotFoundException("group name", groupName));
+        return groupRepository.findByGroupName(groupName).orElseThrow(()->new GroupNotFoundException("group name", groupName));
     }
 
     public List<Group> findAllGroups(){
-        List<Group> groups = repository.findAll();
+        List<Group> groups = groupRepository.findAll();
         if(groups.isEmpty())
             throw new NoDataFoundException();
         return groups;
     }
 
     public List<Group> findByGroupLanguage(String language) {
-        List<Group> groups = repository.findByLanguages(language);
+        List<Group> groups = groupRepository.findByLanguages(language);
         if (groups.isEmpty()) {
             throw new GroupNotFoundException("group language", language);
+        }
+        return groups;
+    }
+
+    public void save(Group group){
+        groupRepository.save(group);
+    }
+
+    public List<Group> findMatchedGroups(long id) {
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User ID", String.valueOf(id)));
+
+        String languages = user.getLanguages().stream().collect(Collectors.joining(",", "{", "}"));
+
+        List<Group> groups = groupRepository.findBestMatchedGroups(user.getTypeOfIllness(), languages, user.getLocation());
+
+        if (groups.isEmpty())
+            groups = groupRepository.findByTypeOfIllnessAndLocationOrLanguages(user.getTypeOfIllness(), languages, user.getLocation());
+
+        if (groups.isEmpty())
+            throw new GroupNotFoundException("No groups available for the user", user.getFirstName());
+
+        return groups;
+    }
+
+    public List<Group> findByTypeOfIllnessAndLocation(String illness, String location){
+        List<Group> groups = groupRepository.findByTypeOfIllnessAndLocation(illness, location);
+        if (groups.isEmpty()) {
+            throw new GroupNotFoundException("Illness:" + illness + "and", "Location:" + location);
+        }
+        return groups;
+    }
+
+    public List<Group> findByIllness(String illness){
+        List<Group> groups = groupRepository.findByTypeOfIllness(illness);
+        if (groups.isEmpty()) {
+            throw new GroupNotFoundException("Illness", illness);
+        }
+        return groups;
+    }
+
+    public List<Group> findByLocation(String location){
+        List<Group> groups = groupRepository.findByLocation(location);
+        if (groups.isEmpty()) {
+            throw new GroupNotFoundException("Location", location);
+        }
+        return groups;
+    }
+
+    public List<Group> findByIllnessAndLocation(String illness, String location){
+        List<Group> groups = groupRepository.findByTypeOfIllnessAndLocation(illness, location);
+        if (groups.isEmpty()) {
+            throw new GroupNotFoundException("Illness-" + illness, "Location-" + location);
+        }
+        return groups;
+    }
+
+    public List<Group> findByIllnessAndLanguage(String illness, String language){
+        List<Group> groups = groupRepository.findByIllnessAndLanguage(illness, language);
+        if (groups.isEmpty()) {
+            throw new GroupNotFoundException("Illness-" + illness , "Language-" + language);
         }
         return groups;
     }
